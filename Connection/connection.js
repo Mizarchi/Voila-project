1278d5cc7aaa09624f7d2c5e0e7d5b113f6b438c
AnDev2804 <mendozaluisandre030@gmail.com>
2024-10-05 11:45:07 -0400

    Cambios Realizados

--- Connection/connection.js
+++ Connection/connection.js
@@ -1,14 +1,13 @@
 const { Sequelize } = require('sequelize');
 
 const database = "voila";
-const username = "root";
-const password = "";
+const username = "postgres";
+const password = "passwd";
 const host = "localhost";
 
 const sequelize = new Sequelize(database, username, password, {
   host: host,
-  dialect: 'mysql',
-  dialectModule: require('mysql2'), // Especifica el módulo de dialecto
+  dialect: 'postgre',
 });
 
 module.exports = {
